cba160262c162e9e868c3bd27934f6d5de78df41
shubham <shubham190602@gmail.com>
2024-10-07 16:57:56 +0530

    Changed the color of navbar and mobileNav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,12 +7,12 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
 
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex-between fixed z-50 w-full bg-[#0e2645] px-6 py-4 lg:px-10'>
       <Link href='/' className='flex items-center gap-1'>
       <Image
           src="/icons/logo1.svg"
-          width={32}
-          height={32}
+          width={38}
+          height={38}
           alt='Interview Nest'
           className='max-sm:size-10'/>
           <p className="text-[26px] font-extrabold text-white max-sm:hidden">
